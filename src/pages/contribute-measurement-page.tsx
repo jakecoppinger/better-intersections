b11b1bee125df80def66a41a91a54c76ee7173ea
Jake Coppinger <jake@jakecoppinger.com>
2023-09-16 01:33:33 +1000

    WIP OTP code auth flow working
    (as opposed to magic link flow)

--- src/pages/contribute-measurement-page.tsx
+++ src/pages/contribute-measurement-page.tsx
@@ -4,17 +4,16 @@ import { createClient, Session } from "@supabase/supabase-js";
 import { supabase } from "../utils/supabase-client";
 import { AuthenticatedForm } from "../components/AuthenticatedContributeMeasurementForm";
 
-
+type AuthState = "no-login-email-sent" | "sending-email" | "waiting-on-code" | "checking-code" | "logged-in";
 export const ContributeMeasurementPage: React.FC = () => {
-  const [loading, setLoading] = useState(false)
-  const [username, setUsername] = useState(null)
-  const [website, setWebsite] = useState(null)
-  const [avatar_url, setAvatarUrl] = useState(null)
-  const [session, setSession] = useState<Session | null>(null)
+  const [authState, setAuthState] = useState<AuthState>("no-login-email-sent");
+  const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
     async function getDBSession() {
-      const {data: {session}} = await supabase.auth.getSession();
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
       setSession(session);
 
       supabase.auth.onAuthStateChange((_event, session) => {
@@ -24,21 +23,8 @@ export const ContributeMeasurementPage: React.FC = () => {
     getDBSession();
   }, []);
 
-  const [email, setEmail] = useState('')
-
-  const handleLogin = async (event: any) => {
-    event.preventDefault()
-
-    setLoading(true)
-    const { error } = await supabase.auth.signInWithOtp({ email })
-
-    if (error) {
-      alert(error.message)
-    } else {
-      alert('Check your email for the login link!')
-    }
-    setLoading(false)
-  }
+  const [email, setEmail] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
 
   return (
     <HeaderAndFooter>
@@ -71,29 +57,93 @@ export const ContributeMeasurementPage: React.FC = () => {
           </a>
         </p>
 
+          {!session && (authState === "no-login-email-sent" || authState === "sending-email") && 
+            <form
+              className="form-widget"
+              onSubmit={async (event: any) => {
+                event.preventDefault();
+                setAuthState("sending-email");
+                const { error } = await supabase.auth.signInWithOtp({ email });
+                if (error) {
+                  alert(error.message);
+                } else {
+                  alert("Check your email for the login code!");
+                }
+                setAuthState("waiting-on-code");
+              }}
+            >
+              <div>
+                <input
+                  className="inputField"
+                  type="email"
+                  placeholder="Your email"
+                  value={email}
+                  required={true}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div>
+                <button
+                  className={"button block"}
+                  disabled={authState === "sending-email"}
+                >
+                  {authState === "sending-email" ? (
+                    <span>Sending code</span>
+                  ) : (
+                    <span>Send login code</span>
+                  )}
+                </button>
+              </div>
+            </form>
+            }
+          
+          {!session && (authState === "waiting-on-code" || authState === "checking-code" )&&
+            <div>
+              <h1>Enter your code</h1>
+            <form
+              className="form-widget"
+              onSubmit={async (event: any) => {
+                event.preventDefault();
+                setAuthState("checking-code");
+                const { error } = await supabase.auth.verifyOtp({
+                  email,
+                  type: 'magiclink',
+                  token: verificationCode
+                });
+                if (error) {
+                  alert(error.message);
+                  return;
+                }
+                alert("You're logged in!");
+                setAuthState("logged-in");
+              }}
+            >
+              <div>
+                <input
+                  className="inputField"
+                  type="text"
+                  placeholder="Verification code"
+                  value={verificationCode}
+                  required={true}
+                  onChange={(e) => setVerificationCode(e.target.value)}
+                />
+              </div>
+              <div>
+                <button
+                  className={"button block"}
+                  disabled={authState === "checking-code"}
+                >
+                  {authState === "checking-code" ? (
+                    <span>Verifing code</span>
+                  ) : (
+                    <span>Check verification code</span>
+                  )}
+                </button>
+              </div>
+            </form>
+            </div> }
 
-        {!session ? 
-        
-        <form className="form-widget" onSubmit={handleLogin}>
-          <div>
-            <input
-              className="inputField"
-              type="email"
-              placeholder="Your email"
-              value={email}
-              required={true}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <button className={"button block"} disabled={loading}>
-              {loading ? <span>Loading</span> : <span>Send magic link</span>}
-            </button>
-          </div>
-        </form>
-        
-        
-        : <AuthenticatedForm key={session.user.id} session={session} />}
+        {session && <AuthenticatedForm key={session.user.id} session={session} />}
 
         {/* {!isLoaded && <p>Loading authentication...</p>}
         {isLoaded && !isSignedIn && <SignInButton />}
