5b04e05709f03192efbc453b17fd014e240cccb2
Jake Coppinger <jake@jakecoppinger.com>
2023-09-15 22:45:53 +1000

    Improve instruction copy

--- src/pages/contribute-measurment.tsx
+++ src/pages/contribute-measurment.tsx
@@ -20,15 +20,38 @@ const ContributeMeasurement: React.FC = () => {
       <div>
         <h1>Contribute Measurement</h1>
 
-        <p>Intro on how to measure is here</p>
+        <p>
+          Help find intersections that need better priority for pedestrians and
+          cyclists!
+        </p>
+
+        <p>
+          By submitting this form you are happy for your answers to be published
+          as open data under ODbL.
+        </p>
+
+        <p>
+          Email address is required to identify sources of spam (so that it can
+          be batch-removed by email), however email will not be published.
+        </p>
+
+        <p>
+          View all data on the{" "}
+          <a 
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://docs.google.com/spreadsheets/d/1L08GNolPYjiRwLOL2d3lAZPqwCNe5vGr6SAOtH7hnNM/edit#gid=0">
+            Google Sheet
+          </a>
+        </p>
 
         {!isLoaded && <p>Loading authentication...</p>}
         {isLoaded && !isSignedIn && <SignInButton />}
         {isLoaded && isSignedIn && (
           <div>
+            <h2>Contribute a measurement</h2>
             <p>You are now signed in as {email}!</p>
 
-            <h2>Here goes the questions and form submit</h2>
 
             <SignOutButton></SignOutButton>
           </div>
