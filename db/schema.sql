14be3c96fae40d1427b3a39d4105daec97d18cdb
Jake Coppinger <jake@jakecoppinger.com>
2023-09-23 23:28:03 +1000

    Handle new user trigger is now longer needed

--- db/schema.sql
+++ db/schema.sql
@@ -46,20 +46,3 @@ create policy "Users can insert their own measurement." on measurements
 
 create policy "Users can update own measurement." on measurements
   for update using (auth.uid() = id);
-
--- This trigger automatically creates a profile entry when a new user signs up via Supabase Auth.
--- See https://supabase.com/docs/guides/auth/managing-user-data#using-triggers for more details.
-
-
-create or replace function public.handle_new_user()
-returns trigger as $$
-begin
-  insert into public.measurements (id)
-  values (new.id);
-  return new;
-end;
-$$ language plpgsql security definer;
-
-create or replace trigger on_auth_user_created
-  after insert on auth.users
-  for each row execute procedure public.handle_new_user();
