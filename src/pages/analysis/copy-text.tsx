526ee3a9265a4f567eea40c5199d0279eeb41d4b
Jake Coppinger <jake@jakecoppinger.com>
2025-09-17 10:13:35 +1000

    WIP copy text, based from measurements

--- src/pages/analysis/copy-text.tsx
+++ src/pages/analysis/copy-text.tsx
@@ -1,109 +1,158 @@
 import { Link } from "react-router-dom";
 
 export const InitialPageText = () => <>
-      <p>An expansion of the Better Intersections Cinematic Universe™️</p>
+  <p>An expansion of the Better Intersections Cinematic Universe™️</p>
 
-      <p>
-        {" "}
-        These are a collection of charts picking apart the Better Intersections
-        dataset. They provide multiple avenues to find further patterns in
-        complex and incomplete data, but also as a tool for communicating and
-        demonstrating improvement over time (or perhaps lack thereof).
-      </p>
+  <p>
+    {" "}
+    These are a collection of charts picking apart the Better Intersections
+    dataset. They provide multiple avenues to find further patterns in
+    complex and incomplete data, but also as a tool for communicating and
+    demonstrating improvement over time (or perhaps lack thereof).
+  </p>
 
-      <p>
-        I've intentionally added charts to demonstrate the limitations of the
-        current accuracy and coverage of the dataset. Please note that the data
-        is crowdsourced by a relatively small number of volunteers - for which I
-        am incredibly grateful to those who have contributed! - and as such
-        should not be used as a primary source of truth for any decision making
-        (yet) - but a useful tool for further investigation nonetheless!
-      </p>
+  <p>
+    I've intentionally added charts to demonstrate the limitations of the
+    current accuracy and coverage of the dataset. Please note that the data
+    is crowdsourced by a relatively small number of volunteers - for which I
+    am incredibly grateful to those who have contributed! - and as such
+    should not be used as a primary source of truth for any decision making
+    (yet) - but a useful tool for further investigation nonetheless!
+  </p>
 
-      <p>
-        All code used to generate these charts is{" "}
-        <Link
-          to="https://github.com/jakecoppinger/better-intersections/blob/main/src/pages/analysis.tsx"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open source on Github
-        </Link>
-        . Contributions are very welcome! Please raise an issue if you find any
-        bugs, or feel free to contact me via email (
-        <Link to={"mailto:jake@jakecoppinger.com"}>jake@jakecoppinger.com</Link>
-        ) or <Link to={"https://mastodon.social/@jakecoppinger"}>Mastodon</Link>
-        .
-      </p>
-      <p>
-        Please note: this is a living document and is still in a draft stage - I
-        wrote it in 2 days.
-      </p>
+  <p>
+    All code used to generate these charts is{" "}
+    <Link
+      to="https://github.com/jakecoppinger/better-intersections/blob/main/src/pages/analysis.tsx"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      open source on Github
+    </Link>
+    . Contributions are very welcome! Please raise an issue if you find any
+    bugs, or feel free to contact me via email (
+    <Link to={"mailto:jake@jakecoppinger.com"}>jake@jakecoppinger.com</Link>
+    ) or <Link to={"https://mastodon.social/@jakecoppinger"}>Mastodon</Link>
+    .
+  </p>
+  <p>
+    Please note: this is a living document and is still in a draft stage</p>
 
-      <h4>Data sources, implementation and caching concerns</h4>
-      <p>
-        All geographic data is from OpenStreetMap. All measurement data is from
-        Better Intersections. Charts are generated using{" "}
-        <Link
-          to="https://observablehq.com/plot/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Observable Plot
-        </Link>
-        .
-      </p>
+  <h4>How long do pedestrians wait at traffic signals around the world?</h4>
+  <p>Below are some rough estimates of the maximum wait time for pedestrians
+    at traffic lights around the world.
+    Please see the <a href="/">map</a> for source measurements.</p>
+  <p>Note that whether pedestrians have a <i>protected phase</i>
+    has a large impact on the wait time. Cities that have
+    reduced traffic volumes and vehicle speeds to 30km/h achieve safe streets
+    without protected pedestrian phases (eg. Copenhagen). Other cities have advanced
+    signal phasing to prioritise pedestrians using multi stage crossings to permit
+    pedestrians to cross whenever possible.
+  </p>
+  <p> Max pedestrian waits:</p>
+  <p><a href="https://www.cityofsydney.nsw.gov.au/strategies-action-plans/city-walking-strategy-action-plan-continuing-vision">
+    City of Sydney target:</a> maximum wait time for pedestrians of 45 seconds with a target of 30 seconds.</p>
+    <p>Corrections to this list are welcome - please <a href="mailto:jake@jakecoppinger.com">contact me</a>,
+    <a href="https://betterintersections.jakecoppinger.com/contribute-measurement">contribute a measurement</a>,
+    <a href="https://github.com/jakecoppinger/better-intersections/blob/main/src/pages/analysis/copy-text.tsx">submit a PR to 
+    edit this page</a>, or release open signal timing data for your city.
+    </p>
 
-      <p>
-        Generating these charts requires a large number of queries to Overpass
-        Turbo to fetch OpenStreetMap data (eg. fetching all traffic lights for
-        every council in Sydney). To mitigate load on these community-run
-        servers, all requests are made and cached to the Better Intersections
-        database at compile time.
-      </p>
-      <p>
-        Any intersections added since the last build will trigger client-side
-        requests to the OSM API, with the exception of categorising
-        intersections by council. This means any measurements since the latest
-        build within Sydney will not appear in charts that filter results by
-        Sydney specifically.
-      </p>
+  <p>
+    {
+      /* - **SCATS**
+    - Sydney CBD: 80-110 seconds
+    - Singapore: 126s
+    - Queenstown: 85-107s
+      - **Non-SCATS**
+    - Amsterdam: 24-71s (overestimate)
+    - Paris: 35-60s
+    - Copenhagen: 45s
+    - Aarhus: 47-52s
+    - Oslo: 33s (68 over arterial)
+    - Barcelona: 59s
+    - Utrecht: 20-40s (overestimate) */}
+    <ul>
+      <li>Cities using SCATS</li>
+      <ul>
+        <li>Sydney CBD: 80-110 seconds max wait (90-100s cycle time)</li>
+        <li>Singapore: 105-126s max wait (
+          <a href="https://betterintersections.jakecoppinger.com/intersection/node/7926827155">115</a>-<a href="https://betterintersections.jakecoppinger.com/intersection/node/6208991348">
+        146s</a> cycle time)</li>
+        <li>Queenstown: 85-107s max wait (<a href="https://betterintersections.jakecoppinger.com/intersection/node/12663542204">90</a>-<a href="https://betterintersections.jakecoppinger.com/intersection/node/11005599123">
+        113s</a> cycle time)</li>
+      </ul>
 
-      <h2>Notes on best practice</h2>
-      <p>
-        More comprehensive details are at{" "}
-        <Link
-          target="_blank"
-          rel="noopener noreferrer"
-          to="https://jakecoppinger.com/2023/07/shining-a-light-on-the-traffic-signals-of-sydney/"
-        >
-          Shining a Light on the Traffic Signals of Sydney (July 2023)
-        </Link>
-        .
-      </p>
-      <p>
-        The excellent City of Sydney{" "}
-        <Link to="https://www.cityofsydney.nsw.gov.au/strategies-action-plans/city-walking-strategy-action-plan-continuing-vision">
-          "A City for Walking: Strategy and Action Plan - Continuing the Vision"
-        </Link>{" "}
-        draft states action 4 (pg. 36) is:
-      </p>
-      <p>
-        <blockquote>
-          We will work with Transport for NSW to ensure that signal phasing
-          prioritises people walking. The City will advocate for:{" "}
-          <ul>
-            <li>Automated pedestrian phases </li>
-            <li>
-              A maximum wait time at intersections of 45 seconds for people
-              walking with a target of 30 seconds
-            </li>
-          </ul>
-        </blockquote>
-      </p>
-      <p>
-        I've added lines that display these best practice benchmarks where the
-        maximum wait time is displayed (calculated here as the sum of the
-        flashing red and solid red durations for the same traffic light sample).
-      </p>
+    </ul>
+
+
+
+  </p>
+
+  <h4>Data sources, implementation and caching concerns</h4>
+  <p>
+    All geographic data is from OpenStreetMap. All measurement data is from
+    Better Intersections. Charts are generated using{" "}
+    <Link
+      to="https://observablehq.com/plot/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Observable Plot
+    </Link>
+    .
+  </p>
+
+  <p>
+    Generating these charts requires a large number of queries to Overpass
+    Turbo to fetch OpenStreetMap data (eg. fetching all traffic lights for
+    every council in Sydney). To mitigate load on these community-run
+    servers, all requests are made and cached to the Better Intersections
+    database at compile time.
+  </p>
+  <p>
+    Any intersections added since the last build will trigger client-side
+    requests to the OSM API, with the exception of categorising
+    intersections by council. This means any measurements since the latest
+    build within Sydney will not appear in charts that filter results by
+    Sydney specifically.
+  </p>
+
+  <h2>Notes on best practice</h2>
+  <p>
+    More comprehensive details are at{" "}
+    <Link
+      target="_blank"
+      rel="noopener noreferrer"
+      to="https://jakecoppinger.com/2023/07/shining-a-light-on-the-traffic-signals-of-sydney/"
+    >
+      Shining a Light on the Traffic Signals of Sydney (July 2023)
+    </Link>
+    .
+  </p>
+  <p>
+    The excellent City of Sydney{" "}
+    <Link to="https://www.cityofsydney.nsw.gov.au/strategies-action-plans/city-walking-strategy-action-plan-continuing-vision">
+      "A City for Walking: Strategy and Action Plan - Continuing the Vision"
+    </Link>{" "}
+    draft states action 4 (pg. 36) is:
+  </p>
+  <p>
+    <blockquote>
+      We will work with Transport for NSW to ensure that signal phasing
+      prioritises people walking. The City will advocate for:{" "}
+      <ul>
+        <li>Automated pedestrian phases </li>
+        <li>
+          A maximum wait time at intersections of 45 seconds for people
+          walking with a target of 30 seconds
+        </li>
+      </ul>
+    </blockquote>
+  </p>
+  <p>
+    I've added lines that display these best practice benchmarks where the
+    maximum wait time is displayed (calculated here as the sum of the
+    flashing red and solid red durations for the same traffic light sample).
+  </p>
 </>;
